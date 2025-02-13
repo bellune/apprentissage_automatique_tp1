03f1e8b82d0b398af3d74d5bc2886fb0525daf87
Bellune Tabitha Megane <bellunetabithamegane@gmail.com>
2025-02-13 01:25:16 -0500

    test et entrainement

--- config.py
+++ config.py
@@ -6,19 +6,19 @@ from sklearn.naive_bayes import GaussianNB
 #Donnees Equilibrees
 models = {
         "Decision Tree": DecisionTreeClassifier(random_state=42),
-        "Bagging (50 arbres)": BaggingClassifier(estimator=DecisionTreeClassifier(),
-                                                  n_estimators=50, random_state=42),
-        "AdaBoost (50 arbres)": AdaBoostClassifier(estimator=DecisionTreeClassifier(max_depth=1),
-                                                    n_estimators=50, learning_rate=1.0, algorithm="SAMME",random_state=42),
+        "Bagging": BaggingClassifier(estimator=DecisionTreeClassifier(),
+                                                  n_estimators=100, random_state=42),
+        "AdaBoost": AdaBoostClassifier(estimator=DecisionTreeClassifier(max_depth=3),
+                                                    n_estimators=100, learning_rate=1.0, algorithm="SAMME",random_state=42),
         "Gradient Boosting": GradientBoostingClassifier(n_estimators=100, learning_rate=0.1, max_depth=3),
         "Random Forest": RandomForestClassifier(n_estimators=100, max_depth=3),
         "Naives baysien":  GaussianNB() }
 
 models_unbalance = {
         "Decision Tree": DecisionTreeClassifier(random_state=42),
-        "Bagging (50 arbres)": BaggingClassifier(estimator=DecisionTreeClassifier(),
+        "Bagging": BaggingClassifier(estimator=DecisionTreeClassifier(),
                                                   n_estimators=50, random_state=42),
-        "AdaBoost (50 arbres)": AdaBoostClassifier(estimator=DecisionTreeClassifier(max_depth=1),
+        "AdaBoost": AdaBoostClassifier(estimator=DecisionTreeClassifier(max_depth=1),
                                                     n_estimators=50, learning_rate=1.0, algorithm="SAMME",random_state=42),
         "Gradient Boosting": GradientBoostingClassifier(n_estimators=100, learning_rate=0.1, max_depth=3),
         "Random Forest": RandomForestClassifier(n_estimators=100, max_depth=3),
